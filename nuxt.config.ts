dfb95bca4901219b98869211728cb45f6cfdfabe
Pavel Semenov <rulevoyzp@gmail.com>
2023-06-27 18:29:35 +0300

    module test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+import {createResolver} from "@nuxt/kit";
+
+const { resolve } = createResolver(import.meta.url)
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/color-mode',
     '@vueuse/nuxt',
     '@pinia/nuxt',
+    resolve('./modules/auth'),
   ],
 
   pinia: {
