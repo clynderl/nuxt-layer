83d74a0337d6624d1f60b1699edfb205d90b8c5a
Pavel Semenov <rulevoyzp@gmail.com>
2023-06-27 18:49:26 +0300

    remove module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
-const currentDir = dirname(fileURLToPath(import.meta.url))
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/color-mode',
     '@vueuse/nuxt',
     '@pinia/nuxt',
-    join(currentDir, 'app/auth'),
   ],
 
   pinia: {
