8a3bd92f4e202d58bd07d078ca1ff384f092bd60
Pavel Semenov <rulevoyzp@gmail.com>
2023-06-27 18:46:38 +0300

    module test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+const currentDir = dirname(fileURLToPath(import.meta.url))
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/color-mode',
     '@vueuse/nuxt',
     '@pinia/nuxt',
+    join(currentDir, 'app/auth'),
   ],
 
   pinia: {
