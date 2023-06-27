a9a1fba834eca0d70bdb86499110b600a37f29e8
Pavel Semenov <rulevoyzp@gmail.com>
2023-06-27 18:26:33 +0300

    module test

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -16,5 +16,5 @@ export function useGet(endpoint: NitroFetchRequest, params = {}) {
 export const useBasedFetch: typeof useFetch = (request, opts?) => {
   const config = useRuntimeConfig();
 
-  return useFetch(request, {baseURL: config.public.backendBaseUrl, ...opts});
+  return useFetch(request, {baseURL: config.public.backendBaseUrl as string, ...opts});
 };
---
+++ modules/auth/index.ts
@@ -0,0 +1,14 @@
+import {addPlugin, createResolver, defineNuxtModule} from "@nuxt/kit";
+
+
+export default defineNuxtModule({
+  meta: {
+    name: 'auth',
+    configKey: 'auth',
+  },
+  setup(options, nuxt) {
+    const { resolve } = createResolver(import.meta.url)
+
+    addPlugin(resolve('./runtime/plugin'));
+  }
+});
