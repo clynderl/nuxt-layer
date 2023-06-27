f46ec54dcaf1992a74ce6f6b35cf0e1356219f8a
Pavel Semenov <rulevoyzp@gmail.com>
2023-06-27 14:59:14 +0300

    modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,14 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/color-mode',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+  ],
+
+  pinia: {
+    autoImports: [
+      'defineStore', ['defineStore', 'definePiniaStore'],
+    ],
+  },
 })
