c1505bae8f6b26b440b8b505a2929781089121bd
Pavel Semenov <rulevoyzp@gmail.com>
2023-06-27 18:31:02 +0300

    module test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-import {createResolver} from "@nuxt/kit";
+import {createResolver} from "nuxt/kit";
 
 const { resolve } = createResolver(import.meta.url)
 export default defineNuxtConfig({
